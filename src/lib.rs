46b5f02e2ba6c1e5860451698d7e92299f439964
Steen Larsen <sla@keycore.dk>
2023-12-27 13:15:04 +0100

    fixed tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,6 @@ pub trait TStateMachine<S, E, A> {
     fn new(states: Vec<S>, events: Vec<E>, transitions: HashMap<(E, S), StateResult<S, A>>) -> Self
     where
         Self: Sized;
-    // fn new(states: Vec<S>, events: Vec<E>, transitions: Vec<Vec<StateResult<S, A>>>) -> Self
-    // where
-    //     Self: Sized;
     fn update_state(&mut self, event: E);
     fn current_state(&self) -> &StateResult<S, A>;
 }
@@ -24,7 +21,7 @@ pub struct StateResult<S, A> {
 #[derive(Clone)]
 pub struct StateMachine<S, E, A> {
     state: StateResult<S, A>,
-    transitions: HashMap<(E, S), StateResult<S, A>>, //Vec<Vec<StateResult<S, A>>>,
+    transitions: HashMap<(E, S), StateResult<S, A>>,
     states: Vec<S>,
     events: Vec<E>,
 }
@@ -45,20 +42,6 @@ where
         let new_state = self.transitions.get(&(event, self.state.state.clone())).unwrap_or(&alt_state);
         self.state = new_state.clone()
     }
-    // fn new(states: Vec<S>, events: Vec<E>, transitions: Vec<Vec<StateResult<S, A>>>) -> Self {
-    //     StateMachine { state: StateResult { state: S::default(), actions: Vec::new() }, transitions, states, events }
-    // }
-
-    // fn update_state(&mut self, event: E) {
-    //     println!("UPDATING");
-    //     let ei = self.events.iter().position(|e| e == &event);
-    //     let si = self.states.iter().position(|s| s == &self.state.state);
-    //     println!("EI {:#?} SI {:#?}", ei, si);
-    //     if let (Some(si), Some(ei)) = (si, ei) {
-    //         println!("EI2 {} SI2 {}", ei, si);
-    //         self.state = self.transitions[ei][si].clone();
-    //     }
-    // }
 
     fn current_state(&self) -> &StateResult<S, A> {
         &self.state
@@ -70,9 +53,9 @@ mod tests {
     use super::*;
     use std::sync::LazyLock;
     use strum::IntoEnumIterator;
-    use strum_macros::EnumIter;
+    use strum_macros::{EnumDiscriminants, EnumIter};
 
-    #[derive(Debug, EnumIter, Default, PartialEq, Eq, Clone)]
+    #[derive(Debug, EnumIter, Default, PartialEq, Eq, Clone, Hash)]
     enum State {
         #[default]
         Empty,
@@ -82,53 +65,63 @@ mod tests {
         Failed,
     }
 
-    #[derive(Debug, EnumIter, Clone)]
+    #[derive(Debug, EnumIter, Clone, Default)]
     enum Action {
+        #[default]
         AddItem,
         DeleteItem,
         Pay,
         RefundDiff,
     }
-    #[derive(Debug, EnumIter, PartialEq, Eq, Clone)]
+    #[derive(Debug, EnumIter, PartialEq, Eq, Clone, EnumDiscriminants)]
+    #[strum_discriminants(derive(EnumIter, Hash))]
     enum Event {
         ItemAdded,
         ItemDeleted,
         OrderPayed,
     }
 
-    static TRANSITIONS: LazyLock<Vec<Vec<StateResult<State, Action>>>> = LazyLock::new(|| {
-        vec![
-            vec![
-                /* ItemAdded */
-                StateResult { state: State::InProgress, actions: vec![Action::AddItem, Action::DeleteItem] }, //Empty
-                StateResult { state: State::InProgress, actions: vec![Action::AddItem, Action::DeleteItem] }, //InProgress
-                StateResult { state: State::PayDiff, actions: vec![Action::Pay] },                            //Payed
-            ],
-            vec![
-                /* ItemDeleted */
-                StateResult { state: State::Failed, actions: vec![Action::AddItem, Action::DeleteItem] }, //Empty
-                StateResult { state: State::InProgress, actions: vec![] },                                //InProgress
-                StateResult { state: State::Payed, actions: vec![Action::RefundDiff] },                   //Payed
-            ],
-            vec![
-                /* OrderPayed */
-                StateResult { state: State::Failed, actions: vec![] }, //Empty
-                StateResult { state: State::Payed, actions: vec![] },  //InProgress
-                StateResult { state: State::Failed, actions: vec![] }, //Payed
-            ],
-        ]
+    static TRANSITIONS: LazyLock<HashMap<(EventDiscriminants, State), StateResult<State, Action>>> = LazyLock::new(|| {
+        let mut map: HashMap<(EventDiscriminants, State), StateResult<State, Action>> = HashMap::new();
+
+        /* ItemAdded */
+        map.insert(
+            (EventDiscriminants::ItemAdded, State::Empty),
+            StateResult { state: State::InProgress, actions: vec![Action::AddItem, Action::DeleteItem] },
+        );
+        map.insert(
+            (EventDiscriminants::ItemAdded, State::InProgress),
+            StateResult { state: State::InProgress, actions: vec![Action::AddItem, Action::DeleteItem] },
+        );
+        map.insert((EventDiscriminants::ItemAdded, State::Payed), StateResult { state: State::PayDiff, actions: vec![Action::Pay] });
+        /* ItemDeleted */
+        map.insert(
+            (EventDiscriminants::ItemDeleted, State::Empty),
+            StateResult { state: State::Failed, actions: vec![Action::AddItem, Action::DeleteItem] },
+        );
+        map.insert(
+            (EventDiscriminants::ItemDeleted, State::InProgress),
+            StateResult { state: State::InProgress, actions: vec![Action::AddItem, Action::DeleteItem] },
+        );
+        map.insert((EventDiscriminants::ItemDeleted, State::Payed), StateResult { state: State::Payed, actions: vec![Action::RefundDiff] });
+        /* OrderPayed */
+        map.insert((EventDiscriminants::OrderPayed, State::Empty), StateResult { state: State::Failed, actions: vec![] });
+        map.insert((EventDiscriminants::OrderPayed, State::InProgress), StateResult { state: State::Payed, actions: vec![] });
+        map.insert((EventDiscriminants::OrderPayed, State::Payed), StateResult { state: State::Failed, actions: vec![] });
+        map
     });
+
     #[test]
     fn test_machine1() {
-        let fsm = StateMachine::new(State::iter().collect(), Event::iter().collect(), TRANSITIONS.to_owned());
+        let fsm = StateMachine::new(State::iter().collect(), EventDiscriminants::iter().collect(), TRANSITIONS.to_owned());
         assert_eq!(fsm.current_state().state, State::Empty)
     }
     #[test]
     fn test_machine2() {
-        let mut fsm = StateMachine::new(State::iter().collect(), Event::iter().collect(), TRANSITIONS.to_owned());
-        fsm.update_state(Event::ItemAdded);
-        fsm.update_state(Event::ItemAdded);
-        fsm.update_state(Event::OrderPayed);
+        let mut fsm = StateMachine::new(State::iter().collect(), EventDiscriminants::iter().collect(), TRANSITIONS.to_owned());
+        fsm.update_state(EventDiscriminants::ItemAdded);
+        fsm.update_state(EventDiscriminants::ItemAdded);
+        fsm.update_state(EventDiscriminants::OrderPayed);
         assert_eq!(fsm.current_state().state, State::Payed);
         assert!(fsm.current_state().actions.is_empty())
     }
